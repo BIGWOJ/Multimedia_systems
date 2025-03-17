fe2c9734ede302e0714a17e2cc7cb9c2f5fe8bba
BIGWOJ <wojtek.latos@interia.pl>
2025-03-17 22:45:06 +0100

    Lab 1 done

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,6 +4,8 @@ import sounddevice as sd
 import soundfile as sf
 import scipy.fftpack
 
+
+
 def task_1():
     data, fs = sf.read('SOUND_INTRO/sound1.wav', dtype='float32')
     # sd.play(data, fs)
@@ -47,29 +49,71 @@ def task_1():
     plt.plot(np.arange(0, fs / 2, fs / fsize), 20 * np.log10(np.abs(yf[:fsize // 2])))
     plt.show()
 
-def task_2(signal, fs, time_margin=[0, 0.02]):
-    time = np.arange(0, len(signal)) / fs
+def task_2():
+    from useful_modules import generate_figures
+    signal, fs = sf.read('SIN/sin_440Hz.wav', dtype='float32')
+    time_margin = [0.0, 0.005]
+    generate_figures(signal, fs, time_margin)
 
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.plot(time, signal)
-    plt.xlabel('[s]')
-    plt.ylabel('Amplitude')
-    plt.xlim(time_margin)
-    plt.title('Signal')
+def task_3():
+    from docx import Document
+    from docx.shared import Inches
+    from io import BytesIO
 
-    yf = scipy.fftpack.fft(signal)
-    xf = np.linspace(0, fs / 2, len(yf) // 2)
+    files = ['SIN/sin_60Hz.wav', 'SIN/sin_440Hz.wav', 'SIN/sin_8000Hz.wav']
+    document = Document()
+    document.add_heading('Change title', 0)
 
-    plt.subplot(2, 1, 2)
-    plt.plot(xf, 20 * np.log10(np.abs(yf[:len(yf) // 2])))
-    plt.xlabel('[Hz]')
-    plt.ylabel('[dB]')
-    plt.title('1/2 Spectrum')
-    plt.tight_layout()
-    plt.show()
+    files = ['SIN/sin_60Hz.wav', 'SIN/sin_440Hz.wav', 'SIN/sin_8000Hz.wav']
+    margins = [[0, 0.02], [0.133, 0.155]]
+    fsizes = [2**8, 2**12, 2**16]
+
+    for file in files:
+        document.add_heading('Plik - {}'.format(file), 2)
+        signal, fs = sf.read(file, dtype='float32')
+        for fsize in fsizes:
+            document.add_heading('Fsize: {}'.format(fsize), 3)
+            fig, axs = plt.subplots(2, 1, figsize=(10, 7))
+
+            # Analizing code, plots
+            ############################################################
+
+            signal_part = signal[:fsize]
+            fft_values = np.fft.fft(signal_part)
+            freqs = np.fft.fftfreq(len(signal_part), d=1/fs)
+            magnitudes = np.abs(fft_values)
+
+            peak_index = np.argmax(magnitudes[:len(magnitudes)//2])
+            peak_freq = freqs[peak_index]
+            peak_amplitude = magnitudes[peak_index]
+
+            axs[0].plot(signal_part)
+            axs[0].set_title("Sygnał")
+            axs[0].set_xlabel("Próbka")
+
+            axs[1].plot(freqs[:len(freqs) // 2], magnitudes[:len(magnitudes) // 2])
+            axs[1].set_title("Widmo")
+            axs[1].set_xlabel("Hz")
+            axs[1].set_ylabel("Amplituda")
+
+            ############################################################
+
+            fig.tight_layout(pad=1.5)
+            memfile = BytesIO()
+            fig.savefig(memfile)
+
+            document.add_picture(memfile, width=Inches(6))
+
+            memfile.close()
+
+            # Text data - values, outputs etc
+            ############################################################
+            document.add_paragraph('Najwyższa częstotliwość = {:.0f}'.format(peak_freq))
+            document.add_paragraph('Wartość amplitudy dla {:.0f}Hz = {:.0f}'.format(peak_freq, peak_amplitude))
+            ############################################################
+
+    document.save('report.docx')
 
 # task_1()(
-
-data, fs = sf.read('SIN/sin_440Hz.wav', dtype='float32')
-task_2(data, fs, )
+# task_2()
+task_3()
---
+++ useful_modules.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_figures(signal, fs, time_margin=[0, 0.02]):
+    import scipy.fftpack
+
+    time = np.arange(0, len(signal)) / fs
+
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(time, signal)
+    plt.xlabel('[s]')
+    plt.ylabel('Amplitude')
+    plt.xlim(time_margin)
+    plt.title('Signal')
+
+    yf = scipy.fftpack.fft(signal)
+    xf = np.linspace(0, fs / 2, len(yf) // 2)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(xf, 20 * np.log10(np.abs(yf[:len(yf) // 2])))
+    plt.xlabel('[Hz]')
+    plt.ylabel('[dB]')
+    plt.title('1/2 Spectrum')
+    plt.tight_layout()
+    plt.show()
+
+def generate_docx_template():
+    from docx import Document
+    from docx.shared import Inches
+    from io import BytesIO
+
+    document = Document()
+    document.add_heading('Change title', 0)
+
+    files = ['sin60Hz.wav', 'sin440Hz.wav', 'sin8000Hz.wav']
+    margins = [[0, 0.02], [0.133, 0.155]]
+
+    for file in files:
+        document.add_heading('Plik - {}'.format(file), 2)
+        for i, Margin in enumerate(margins):
+            document.add_heading('Time margin {}'.format(Margin), 3)
+            fig, axs = plt.subplots(2, 1, figsize=(10, 7))
+
+            ############################################################
+            # Analizing code, plots
+            ############################################################
+
+            fig.suptitle('Time margin {}'.format(Margin))
+            fig.tight_layout(pad=1.5)
+            memfile = BytesIO()
+            fig.savefig(memfile)
+
+            document.add_picture(memfile, width=Inches(6))
+
+            memfile.close()
+            ############################################################
+            # Text data - values, outputs etc
+            document.add_paragraph('Example value = {}'.format(np.random.rand(1)))
+            ############################################################
+
+    document.save('report.docx')
